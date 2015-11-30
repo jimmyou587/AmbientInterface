863cc16bec8b6cd361c8651464b160eac0bb6bde
Jimmy Ouyang <jimmyou587@gmail.com>
2015-11-29 21:57:05 -0800

    Create test case file for Subject class

---
+++ test/SubjectTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SubjectTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRegisterObserver() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnRegisterObserver() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAllObservers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetState() {
+		fail("Not yet implemented");
+	}
+
+}
