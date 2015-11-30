e56c65005d679c62ef129fbc1be1ad067174b39f
Jimmy Ouyang <jimmyou587@gmail.com>
2015-11-29 21:57:33 -0800

    Create test case file for WorkDuration class

---
+++ test/WorkDurationTest.java
@@ -0,0 +1,57 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class WorkDurationTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRegSub() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUnRegSub() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsVibration() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsActive() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDistanceDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDistanceInt() {
+		fail("Not yet implemented");
+	}
+
+}
