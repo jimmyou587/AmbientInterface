d76c407fa3db0684be53cfc5a0e49c82ca6652bf
Jimmy Ouyang <jimmyou587@gmail.com>
2015-12-01 00:50:40 -0800

    Add state definitions

--- src/SoundObserver.java
+++ src/SoundObserver.java
@@ -1,7 +1,16 @@
-import javax.swing.JOptionPane;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import sun.audio.AudioData;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+import sun.audio.ContinuousAudioDataStream;
+
+@SuppressWarnings("restriction")
 public class SoundObserver extends Observer {
-	
+	private static String filePath = "/Users/LO/Documents/workspace/AmbientInterface/bin/beep.wav"; 
+		
 	SoundObserver(Subject sub) {
 		this.subject = sub;
 		this.subject.registerObserver(this);
@@ -11,9 +20,31 @@ public class SoundObserver extends Observer {
 	public void update() {
 		
 //		Update sound
-		System.out.println("Sound Observer observes the state is now: " + subject.getState());
-		JOptionPane.showMessageDialog(null, "Hello");
-		
+	 
+	    // create an audiostream from the inputstream
+	    AudioStream audioStream = null;
+		try {
+			audioStream = new AudioStream(new FileInputStream(filePath));
+			AudioData data = audioStream.getData();
+			ContinuousAudioDataStream cas = new ContinuousAudioDataStream (data);
+			AudioPlayer.player.start(cas);
+			 
+		   	try {
+				Thread.sleep(1000 * this.subject.getState());
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}    	
+		   	AudioPlayer.player.stop(cas);
+		   	
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	 
 	}
 	
 }
