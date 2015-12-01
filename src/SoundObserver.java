abb2e2504762dbd2b1b3530a7249ae63062aa377
Jimmy Ouyang <jimmyou587@gmail.com>
2015-11-30 22:00:11 -0800

    Do register observer in SoundObserver constructor

--- src/SoundObserver.java
+++ src/SoundObserver.java
@@ -1,8 +1,10 @@
+import javax.swing.JOptionPane;
 
 public class SoundObserver extends Observer {
 	
 	SoundObserver(Subject sub) {
 		this.subject = sub;
+		this.subject.registerObserver(this);
 	}
 	
 	@Override
@@ -10,6 +12,7 @@ public class SoundObserver extends Observer {
 		
 //		Update sound
 		System.out.println("Sound Observer observes the state is now: " + subject.getState());
+		JOptionPane.showMessageDialog(null, "Hello");
 		
 	}
 	
